f4f6e12b8484cac15591b59f741cdbf5047f6d21
Stevens Dorchies <Trocme11>
2019-01-16 10:48:00 +0100

    V1

--- create.php
+++ create.php
@@ -11,7 +11,7 @@
 	<form action="" method="post">
 		<div>
 			<label for="name">Name</label>
-			<input type="text" name="name" value="">
+			<input type="text" name="name" id="name" value="">
 		</div>
 
 		<div>
@@ -27,15 +27,15 @@
 		
 		<div>
 			<label for="distance">Distance</label>
-			<input type="text" name="distance" value="">
+			<input type="text" name="distance"id="distance" value="">
 		</div>
 		<div>
 			<label for="duration">Durée</label>
-			<input type="text" name="duration" value="">
+			<input type="text" name="duration" id="duree" value="">
 		</div>
 		<div>
 			<label for="height_difference">Dénivelé</label>
-			<input type="text" name="height_difference" value="">
+			<input type="text" name="height_difference"id="difference" value="">
 		</div>
 		<button type="submit" name="button">Envoyer</button>
 	</form>
@@ -51,6 +51,57 @@ $nom = (isset($_POST['name']) ? $_POST['name'] : null);
 $distance=(isset($_POST['distance']) ? $_POST['distance'] : null);
 $dure=(isset($_POST['duration']) ? $_POST['duration'] : null);
 $difference=(isset($_POST['height_difference']) ? $_POST['height_difference'] : null);
+
+
+
+
+$nom= filter_var($nom,FILTER_SANITIZE_STRING) ? $_REQUEST['name'] : '';
+$level=filter_var($level,FILTER_SANITIZE_STRING) ?$_REQUEST['difficulty']:'Facile';
+$distance=filter_var($distance,FILTER_SANITIZE_NUMBER_INT) ?  $_REQUEST['distance'] : null;
+$dure=filter_var($dure,FILTER_SANITIZE_STRING) ? $_REQUEST['duration'] : null;
+$difference=filter_var($difference,FILTER_SANITIZE_NUMBER_INT)? $_REQUEST['height_difference'] : null;
+
+
+if($nom != filter_var($nom,FILTER_SANITIZE_STRING)) {
+    ?>
+    <style>
+        #name {
+            background-color: red;
+        }
+    </style>
+    <?php
+}
+
+if($distance != filter_var($distance,FILTER_SANITIZE_NUMBER_INT)) {
+    ?>
+    <style>
+        #distance {
+            background-color: red;
+        }
+    </style>
+    <?php
+}
+
+if($dure != filter_var($dure,FILTER_SANITIZE_STRING)) {
+    ?>
+    <style>
+        #duree {
+            background-color: red;
+        }
+    </style>
+    <?php
+}
+
+if($difference != filter_var($difference,FILTER_SANITIZE_NUMBER_INT)) {
+    ?>
+    <style>
+        #difference {
+            background-color: red;
+        }
+    </style>
+    <?php
+}
+
 
 function ajouteRand($table,$nom,$level,$distance,$dure,$difference)
 {
@@ -69,7 +120,7 @@ function ajouteRand($table,$nom,$level,$distance,$dure,$difference)
     $associer->close();
 
 }
-if(isset($_REQUEST['action']) and filter_var($nom,FILTER_SANITIZE_STRING) and filter_var($level,FILTER_SANITIZE_STRING) and filter_var($distance,FILTER_SANITIZE_NUMBER_INT) and filter_var($dure,FILTER_SANITIZE_STRING) and filter_var($difference,FILTER_SANITIZE_NUMBER_INT)) {
+
     ajouteRand('hiking', $nom, $level, $distance, $dure, $difference);
-}
+
 affiche();
--- update.php
+++ update.php
@@ -10,17 +10,79 @@ $duree=(isset($_REQUEST['duration']) ? $_REQUEST['duration'] : null);
 $difference = (isset($_REQUEST['height_difference']) ? $_REQUEST['height_difference'] : null);
 $id =(isset($_REQUEST['id']) ? $_REQUEST['id'] : null);
 
-if(filter_var($name,FILTER_SANITIZE_STRING)){
-   (isset($_REQUEST['name']) ? $_REQUEST['name'] : null);
+function chang($table,$nom, $level, $distance, $duree,$difference,$id)
+{
+    global $conn;
+
+    $sql = "UPDATE $table set  `name`='$nom',`difficulty`='$level',`distance`='$distance',`duration`='$duree',`height_difference`='$difference' where id=$id";
+    echo $sql;
+    $conn->query($sql);
+    echo $conn->error;
 }
-filter_var($level,FILTER_SANITIZE_STRING);
-filter_var($distance,FIL)
+echo'<a href="read.php">Liste des données</a>';
+
+
+
+$name= filter_var($name,FILTER_SANITIZE_STRING) ? $_REQUEST['name'] : '';
+$level=filter_var($level,FILTER_SANITIZE_STRING) ?$_REQUEST['difficulty']:'Facile';
+$distance=filter_var($distance,FILTER_SANITIZE_NUMBER_INT) ?  $_REQUEST['distance'] : null;
+$duree=filter_var($duree,FILTER_SANITIZE_STRING) ? $_REQUEST['duration'] : null;
+$difference=filter_var($difference,FILTER_SANITIZE_NUMBER_INT)? $_REQUEST['height_difference'] : null;
+$id = filter_var($id,FILTER_SANITIZE_NUMBER_INT) ? $_REQUEST['id'] : null;
+
+    chang('hiking', $name, $level, $distance, $duree, $difference, $id);
 
 
 
 
 
 
+
+
+
+
+
+
+
+if($name != filter_var($name,FILTER_SANITIZE_STRING)) {
+    ?>
+    <style>
+        #name {
+            background-color: red;
+        }
+    </style>
+    <?php
+}
+
+    if($distance != filter_var($distance,FILTER_SANITIZE_NUMBER_INT)) {
+    ?>
+    <style>
+        #distance {
+            background-color: red;
+        }
+    </style>
+<?php
+}
+
+    if($duree != filter_var($duree,FILTER_SANITIZE_STRING)) {
+    ?>
+    <style>
+        #duree {
+            background-color: red;
+        }
+    </style>
+<?php
+}
+
+    if($difference != filter_var($difference,FILTER_SANITIZE_NUMBER_INT)) {
+    ?>
+    <style>
+        #difference {
+            background-color: red;
+        }
+    </style>
+<?php
+}
 ?>
 
 
@@ -38,7 +100,7 @@ filter_var($distance,FIL)
 	<form action="update.php" method="post">
 		<div>
 			<label for="name">Name</label>
-			<input type="text" name="name" value="<?=$name?>">
+			<input type="text" name="name" id="name" value="<?=$name?>">
 		</div>
 
 		<div>
@@ -54,15 +116,15 @@ filter_var($distance,FIL)
 		
 		<div>
 			<label for="distance">Distance</label>
-			<input type="text" name="distance" value="<?=$distance?>">
+			<input type="text" name="distance" id="distance" value="<?=$distance?>">
 		</div>
 		<div>
 			<label for="duration">Durée</label>
-			<input type="text" name="duration" value="<?=$duree?>">
+			<input type="text" name="duration" id="duree" value="<?=$duree?>">
 		</div>
 		<div>
 			<label for="height_difference">Dénivelé</label>
-			<input type="text" name="height_difference" value="<?=$difference?>">
+			<input type="text" name="height_difference" id="difference" value="<?=$difference?>">
 
             <input type="hidden" name="id" value="<?=$id?>">
             <input type="hidden" name="action" value="update">
@@ -72,19 +134,3 @@ filter_var($distance,FIL)
 </body>
 </html>
 <?php
-function chang($table,$nom, $level, $distance, $duree,$difference,$id)
-{
-    global $conn;
-
-    $sql = "UPDATE $table set  `name`='$nom',`difficulty`='$level',`distance`='$distance',`duration`='$duree',`height_difference`='$difference' where id=$id";
-    echo $sql;
-    $conn->query($sql);
-    echo $conn->error;
-}
-echo'<a href="read.php">Liste des données</a>';
-
-
-
-if(isset($_REQUEST['action']) and filter_var($name,FILTER_SANITIZE_STRING) and filter_var($level,FILTER_SANITIZE_STRING) and filter_var($distance,FILTER_SANITIZE_NUMBER_INT) and filter_var($duree,FILTER_SANITIZE_STRING) and filter_var($difference,FILTER_SANITIZE_NUMBER_INT)) {
-    chang('hiking', $name, $level, $distance, $duree, $difference, $id);
-}
